6fb386f04ad80c0a339e79ec154d66d5f97fe1ac
TheBigCringeMaster <kristiandgrey@gmail.com>
2021-09-24 12:16:25 +0100

    school 24th #2

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
-import {Address,Page,Button,Div,Text, Title,Image} from "./Converter.js"
+import {UITags,Address,Page,Button,Div,Text, Title,Image} from "./Converter.js"
 
-function PrintHello(){
-    console.log("hello from the website!")
+let page1 = new Page("./main.js","./export.html","./export.css")
+
+function eatPizza(){
+    console.log("yum")
 }
+let btn = new Button("click to eat","eat_class","1","head")
+btn.build()
+btn.configure(["background","red"])
+btn.onClick(eatPizza)
 
-let page = new Page("./main.js", "./export.html", "./export.css")
-
-let div = new Div("div1","div1class","head")
-let title = new Text("hello and this is my new website!", "title1class", "head")
-title.build()
-div.addElement(title)
-let address = new Address("Address1Class","gmail@gmail.com -s- 5731047314","body")
+let address = new Address("pizza_place",`pizza@hotmail.com ${UITags.br} -s- 0814975197`, "body")
 address.build()
-div.build()
-
-let image = new Image("./sussygarlic.PNG","garlicClass","a sussy garlic","body")
-image.build()
-page.addElement(image)
-page.addElement(div)
-page.addElement(address)
-page.load()
+page1.addElement(address)
+page1.addElement(btn)
+page1.load()
